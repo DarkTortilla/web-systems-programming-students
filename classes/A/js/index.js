275eb18aef91aef4609acea2c2858fdffa0a46a0
Darktortilla <elgar-frank@hotmail.com>
2026-08-20 20:47:31 -0600

    introduction to nodejs and js

---
+++ classes/A/js/index.js
@@ -0,0 +1,16 @@
+//primitivos
+
+let name_ = "Frank";
+const num = 5;
+const flag = false;
+const id_1 = Symbol('id');
+const id_2 = Symbol('id');
+
+const none = undefined;
+const null_ = null;
+
+name_ = 5;
+
+console.log('r'+2);
+
+console.log(name_);
---
+++ classes/B/js/index.js
@@ -0,0 +1,15 @@
+let name_ = "Frank";
+name_ = 5;
+
+console.log('r'+5);
+
+// Primitivos
+
+let email = 'example@example.com';
+let phone = 1234567890;
+let flag = true;
+let id1 = Symbol('id');
+let id2 = Symbol('id');
+none = undefined;
+person = null;
+
