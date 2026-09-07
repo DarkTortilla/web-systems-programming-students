f0d21ceaa520f4e7b5f5d15967282a7b7dcad535
Darktortilla <elgar-frank@hotmail.com>
2026-09-07 07:56:14 -0600

    update(c): server with express and nodemon.

--- classes/C/luxe_motors/backend/src/server.js
+++ classes/C/luxe_motors/backend/src/server.js
@@ -1,28 +1,35 @@
-import http from "http";
-// 100
-// 200-299 OK
-// 300-399 redirecciones
-// 400-499 error en el cliente
-// 500-599 error en el servidor
+import express from "express";
+const products = [
+  { id: 1, name: "" },
+  { id: 2, name: "" },
+  { id: 3, name: "" },
+];
 
-export const server = http.createServer((req, res) => {
-  console.log(req.url);
-  switch (req.url) {
-    case "/users":
-      res.writeHead(200, "ok", { "content-type": "application/json" });
-      res.end(JSON.stringify([{ name: "A" }, { name: "B" }, { name: "C" }]));
-      break;
-    case "/home":
-     res.writeHead(200, "ok", { "content-type": "text/html" });
-      res.end("<h1>Hola</h1>");
-      break;
-    default:
-      res.statusCode = 404;
-      res.end("not found");
-      break;
-  }
+const server = express();
+
+server.use(express.json());
+
+server.get("/products", (req, res) => {
+  res.status(200).json(products);
+});
+server.post("/products", (req, res) => {
+  const name = req.body.name;
+  const id = req.body.id;
+  const product = {
+    id,
+    name,
+  };
+  products.push(product);
+  res.status(201).json({ product, message: "ok" });
 });
 
+
+
+
+// server.put();
+// server.patch();
+// server.delete();
+
 server.listen(3000, () => {
-  console.log("server running on port: 3000");
+  console.log("server running on port 3000");
 });
