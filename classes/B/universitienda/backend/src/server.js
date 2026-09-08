13a93776be6bbaa93438d555b04536aef582563f
Darktortilla <elgar-frank@hotmail.com>
2026-09-08 07:15:12 -0600

    update(b) server with express and nodemon

--- classes/B/universitienda/backend/src/server.js
+++ classes/B/universitienda/backend/src/server.js
@@ -1,28 +1,25 @@
-//const http = require('http');
-import http from 'http';
-const users =[{name: 'A'}, {name:'B'}, {name:'C'}]; 
-//fs
+import express from 'express';
+const products = [
+  { id: 1, name: "" },
+  { id: 2, name: "" },
+  { id: 3, name: "" },
+];
+const server = express();
+server.use(express.json());
 
-const server = http.createServer((req, res)=>{
-    console.log(req.url)
-    if (req.url==='/home' && req.method ==='GET') {
-        res.writeHead(200,'ok', {"content-type":"text/html"});
-        //SSR
-        res.end(`
-                <h1>Welcome</h1>
-                <p>${req.url}</p>
-            `);
-    }else if (req.url === '/users') {
-        res.writeHead(200, 'ok', { "content-type":"application/json"});
-        res.end(JSON.stringify(users));
-    }
-    else{
-        res.statusCode=404;
-        res.end('not found');
-    }
+server.get('/products', (req, res)=>{
+    res.json(products);
+});
 
+server.post('/products', (req, res)=>{
+    const id = req.body.id;
+    const name = req.body.name;
+
+    products.push({id, name});
+
+    res.status(201).json({products, message:'ok'});
 });
 
 server.listen(3000, ()=>{
-    console.log("server running on port 3000");
-});
+    console.log('server running on port 3000');
+})
