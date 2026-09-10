4a1b5df046fd1412eaf1fc325551dac68cb3c3ba
Darktortilla <elgar-frank@hotmail.com>
2026-09-09 20:58:44 -0600

    clean architecture c

--- classes/B/universitienda/backend/src/server.js
+++ classes/B/universitienda/backend/src/server.js
@@ -1,46 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import express from "express";
 
-const port = process.env.PORT;
+export class Server {
+  server = express();
 
-const products = [
-  { id: 1, name: "" },
-  { id: 2, name: "" },
-  { id: 3, name: "" },
-];
-const server = express();
-server.use(express.json());
-
-
-server.get('/products', (req, res)=>{
-    res.json(products);
-});
-
-server.post('/products', (req, res)=>{
-    const id = req.body.id;
-    const name = req.body.name;
-
-    products.push({id, name});
-
-    res.status(201).json({products, message:'ok'});
-});
-
-server.put('/products/:id', (req, res)=>{
-   const id = Number(req.params.id);
-   const name = req.body.name;
-
-   const product = products.find(p=>p.id===id);
-   if (!product) {
-        res.status(404).json({message:'product not found'});
-        return;
-   }
-
-   product.name=name;
-   res.status(200).json({message:"product updated"});
-   
-});
-
-server.listen(port, ()=>{
-    console.log(`server running on port: ${port}`);
-});
+  port;
+  constructor(options) {
+    this.port = options.port;
+  }
+  
+  start() {
+    this.server.listen(this.port, () => {
+      console.log(`server running on port: ${this.port}`);
+    });
+  }
+}
