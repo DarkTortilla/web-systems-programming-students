0dcdb3d4003cee6deac3d53f752a958e2a46369a
Darktortilla <elgar-frank@hotmail.com>
2026-09-21 21:02:15 -0600

    update(user) add user controller

---
+++ classes/B/universitienda/backend/src/controllers/user.controller.ts
@@ -0,0 +1,66 @@
+import type { Request, Response } from "express";
+
+const users = [
+  {
+    id: 1,
+    name: "",
+    lastName: "",
+    email: "",
+    role: "",
+    password: "",
+    profileImg: "",
+  },
+  {
+    id: 2,
+    name: "",
+    lastName: "",
+    email: "",
+    role: "",
+    password: "",
+    profileImg: "",
+  },
+];
+
+//CRUD de usuarios: getAll, getById, createUser, 
+// , updateUser
+
+export class UserController {
+  constructor() {}
+  public getUserById(req: Request, res: Response) {
+    // https://store.me.com/api/v1/users/181613513/orders?status=canceled
+    // body: es el cuerpo de la peticion
+    // param
+    // query
+    const id = Number(req.params["id"]);
+
+    const user = users.find((u) => u.id === id);
+    //Javascript object notation
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    res.json(user);
+  }
+
+  public getAllUsers(_req: Request, res: Response) {
+
+    /**
+     * { limit: 10, page: 1}
+     */
+    // const page = req.query['page'];
+    // const limit = req.query['limit'];
+    // const {page,limit} = req.query;
+
+    res.json(users);
+  }
+
+  public createUser(req: Request, res: Response) {
+    
+    const { name, lastName, email, role, password, profileImg } = req.body;
+
+    const id = users[users.length-1]?.id! + 1;
+   
+    users.push( {id, name, lastName, email, role, password, profileImg});
+
+    res.status(201).json({message:'user created'});
+  }
+}
